ef7cac76cddda62a6030d4b177f83f5a07ec98df
daiot <stefanescu.dragos@gmail.com>
2018-09-11 21:47:01 +0300

    worker removed from api

--- Worker/Program.cs
+++ Worker/Program.cs
@@ -6,7 +6,7 @@ namespace Worker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("The job !");
         }
     }
 }
