8241eab422c952e7f96ac17f373b3822fadcad57
daiot <stefanescu.dragos@gmail.com>
2019-01-13 19:40:10 +0200

    added cors for netlify and localhost

--- api/Startup.cs
+++ api/Startup.cs
@@ -134,7 +134,10 @@ namespace CoreJsNoise
             app.UseStaticFiles();
             app.UseMvc();
 
-            app.UseCors(cfg => cfg.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+            app.UseCors(cfg => 
+                cfg.WithOrigins("https://jsnoise.netlify.com","http://localhost:8080")
+
+                .AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
             using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
             {
                 if (serviceScope.ServiceProvider.GetService<PodcastsCtx>() != null)
